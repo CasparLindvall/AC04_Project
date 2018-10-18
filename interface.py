4870e30fd06d0a176d9e67d6a17fd970e831225d
elisabethanna <anna.eriksson94@hotmail.com>
2018-10-18 10:59:12 +0200

    User input

--- interface.py
+++ interface.py
@@ -8,12 +8,24 @@ def alternatives():
 	alt = input("What do you want to do: 1-create system, 2-create worker, 3-remove worker and 4-destroy network: ")
 	if alt == "1":
 		print("Creating system")
+		#createSystem()
 	elif alt == "2":
 		print("Creating worker")
+		#createWorker
 	elif alt == "3":
+		#removeWorker()
 		print("Removing worker")
 	else:
 		print("Destroy network")
+		#destroyNetwork()
+
+def createSystem():
+
+#def createWorker():
+
+#def removeWorker():
+
+#def destroyNetwork():
 
 alternatives()
  
