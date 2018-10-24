bfcb21707b0186a683033d4aa9c79ad90bf1191d
CasparLindvall <caspar08@gmail.com>
2018-10-24 16:51:53 +0000

    store and edit state and worker count

---
+++ init_nodes/editState.py
@@ -0,0 +1,33 @@
+from sys import argv
+
+# Updates the state and worker count
+def updateState(newStatus, workerChange):
+	f = open("state.txt", "r")
+	lines = f.readlines()
+	n_workers = lines[1][-2]
+
+	if(newStatus):
+		lines[0] = ["state="+newStatus+"\n"]
+
+	if(workerChange):
+		lines[1] = ["workers="+str(int(n_workers) + int(workerChange)) +"\n"]
+
+	f = open("state.txt", "w")
+	for line in lines:
+		f.writelines( line )
+# Returns the states
+def getState():
+	f = open("state.txt", "r")
+        lines = f.readlines()
+        n_workers = lines[1][-2]
+	return(lines[0][6:-1], n_workers)
+
+# Can run from cmd if needed
+if __name__ == '__main__':
+
+	if(len(argv) < 2):
+        	print("Too few args!")
+        	exit(1)
+
+	newStatus, workerChange = argv[1], argv[2]
+	updateState(newStatus, workerChange)
