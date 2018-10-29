481d199a1a11f63e8c950d25f0b2dc137cc39783
CasparLindvall <caspar08@gmail.com>
2018-10-29 12:43:56 +0000

    added annas code into editHosts

--- editHosts.py
+++ editHosts.py
@@ -21,10 +21,77 @@ def getIp(name):
 	print(type(ip_adr_list), len(ip_adr_list))
 	return ip_adr_list
 
+
+""" ETC/HOSTS  """
+
+#the filepath is missing sudo permissions
+
+def addWorker():
+    last_no_worker = lastWorker()
+    with open("/etc/hosts", "a") as myfile:
+        myfile.write(x + " sparkworker" + str(int(last_no_worker) + 1) + "\n")
+
+#the filepath is missing sudo permissions
+
+def lastWorker():
+    with open('/etc/hosts', 'r') as myfile:
+        lines = myfile.read().splitlines()
+        last_line = lines[-1]
+        last_no_worker = last_line[-1]
+    return(last_no_worker)
+
+#the filepath is missing sudo permissions
+
+def removeWorker():
+    with open("/etc/hosts", "r+") as infile:
+        lines = infile.read().splitlines()
+        last_worker = lines[-1]
+
+    # havent succeeded removing it but i have identified the last worker :)
+
+
+""" ETC/ANSIBLE/HOSTS  """
+
+# Adding into the second place in etc/ansible/hosts
+
+
+def addWorkerAns():
+    last_no_worker = lastWorkerAns()
+    with open("/etc/ansible/hosts", "a") as myfile:
+        myfile.write("sparkworker" + str(int(last_no_worker) + 1) +
+                     " ansible_connection=ssh ansible_user=ubuntu" + "\n")
+
+
+def lastWorkerAns():
+    with open('/etc/ansible/hosts', 'r') as myfile:
+        lines = myfile.read().splitlines()
+        last_line = lines[-1].split()
+        first_word = last_line[0]
+        no_worker = first_word[-1]
+    return(no_worker)
+
+# Adding into the first place in etc/ansible/hosts
+
+
+def addWorkerAns2():
+    last_no_worker = lastWorkerAns()
+    f = open("/etc/ansible/hosts", "r")
+    contents = f.readlines()
+    f.close()
+
+    contents.insert(48, "sparkworker"+str(int(last_no_worker)) +
+                    " ansible_ssh_host="+"XXXXXXX" + "\n")
+
+    f = open("/etc/ansible/hosts", "w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
+
 if __name__ == '__main__':
 	# image names to be used
-	if(len(sys.argv) < 2):
-		print("Too few args in editHosts")
-		exit(1)
-	x = getIp(sys.argv[1])
-	print(x)
+#	if(len(sys.argv) < 2):
+#		print("Too few args in editHosts")
+#		exit(1)
+#	x = getIp(sys.argv[1])
+#	print(x)
+	addWorker()
