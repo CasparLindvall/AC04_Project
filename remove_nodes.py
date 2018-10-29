012920fd973f6df7789d4617d4d62d818349bf03
CasparLindvall <caspar08@gmail.com>
2018-10-29 11:23:26 +0000

    fixed for whole nw

--- remove_nodes.py
+++ remove_nodes.py
@@ -4,21 +4,23 @@ from editState import updateState, getState
 
 def removeNodes(N):
 	stringReturn = None
-	_, amountWorkers = getState()
+	state, amountWorkers = getState()
 	Nmax = amountWorkers
 
-	if(N > 0 and N <= Nmax):
+	if(N >= -1 and N <= Nmax):
 		if(N == -1):
 			N = Nmax
 			item = sh.nova("delete","ACC4_master1")
 			stringReturn = "You deleted the entire network"
+			state = "shutdown"
+			N = Nmax
 		else:
 			stringReturn = "You deleted "+ str(N) + "workers"
 		for i in range(Nmax, Nmax-N, -1):
 			item = sh.nova("delete","ACC4_worker_"+str(i))
 		amountWorkers -= N
 
-	updateState(workerChange=-N)
+	updateState(state, workerChange=-N)
 	updateHosts(N) # TODO!
 
 	return stringReturn, amountWorkers
@@ -26,11 +28,10 @@ def removeNodes(N):
 
 if __name__ == '__main__':
 
-        if(len(argv) < 2):
+        if(len(sys.argv) < 1):
 		print("Too few args in removeNodes.py")
 		exit(1)
 
-	N    = sys.argv[1] +1
-	Nmax = sys.argv[2]
+	N    = int(sys.argv[1])
 
-	print(removeNodes(N, Nmax))
+	print(removeNodes(N))
