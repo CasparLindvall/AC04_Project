21e8cf477fe0bb17bdc8938df6c5afb767b045ca
CasparLindvall <caspar08@gmail.com>
2018-10-29 10:28:03 +0000

    retrieves ip

--- editHosts.py
+++ editHosts.py
@@ -1,6 +1,24 @@
+import sys
+import sh
+
 #only for worker chamges
 def updateHosts (N=0):
 	print("todo updateHosts")
 
 def initHosts():
 	print("todo init hosts files")
+
+# Get the IP of node type master, worker or server
+def getIp(name):
+
+	node_name = "ACC4_"+name
+
+	item = sh.nova("list")
+	item_row = sh.grep(item, node_name)
+	sh.grep = sh.grep.bake("-Eo")
+	ip_adr = sh.grep(item_row, '\<Network=.*\>')
+	print(ip_adr)
+	#return ip_adr
+
+
+getIp(sys.argv[1])
